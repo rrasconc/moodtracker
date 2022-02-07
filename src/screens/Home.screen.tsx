f3386a2a7cc64a0ad913fd07d3a44664960fb7cc
rigobertorascon <rigobertorascon@Rigobertos-MacBook-Pro.local>
2022-02-06 21:58:15 -0700

    Latest mood display

--- src/screens/Home.screen.tsx
+++ src/screens/Home.screen.tsx
@@ -1,14 +1,19 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import React from 'react';
 import { MoodPicker } from '../components/MoodPicker';
 import { useAppContext } from '../App.provider';
+import { MoodItemRow } from '../components/MoodItemRow';
 
 export const Home: React.FC = () => {
   const appContext = useAppContext();
+  const lastIndex = appContext.moodList.length - 1;
+  const lastMood = appContext.moodList[lastIndex];
 
   return (
     <View style={styles.container}>
       <MoodPicker handleSelectMood={appContext.handleSelectMood} />
+      <Text style={styles.text}>Latest mood</Text>
+      <MoodItemRow item={lastMood} key={lastMood.timestamp} />
     </View>
   );
 };
@@ -17,5 +22,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 13,
+    fontWeight: 'bold',
+    letterSpacing: 1,
+    padding: 10,
   },
 });
